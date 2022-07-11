2b4941643a689212cbb582864e3e535400989cd4
Shouhei Takai <shouheitakai.me@gmail.com>
2022-07-11 22:59:32 +0900

    initial commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,5 @@
-import { ReactElement } from 'react';
-import styled from 'styled-components';
-import { ActivateDeactivate } from './components/ActivateDeactivate';
-import { Greeter } from './components/Greeter';
-import { SectionDivider } from './components/SectionDivider';
-import { SignMessage } from './components/SignMessage';
-import { WalletStatus } from './components/WalletStatus';
-
-const StyledAppDiv = styled.div`
-  display: grid;
-  grid-gap: 20px;
-`;
-
-export function App(): ReactElement {
-  return (
-    <StyledAppDiv>
-      <ActivateDeactivate />
-      <SectionDivider />
-      <WalletStatus />
-      <SectionDivider />
-      <SignMessage />
-      <SectionDivider />
-      <Greeter />
-    </StyledAppDiv>
-  );
-}
+export const App = () => (
+  <article>
+    <h1>Hello world!</h1>
+  </article>
+)
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,11 @@
-import { Web3ReactProvider } from '@web3-react/core';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
-import './index.css';
-import { getProvider } from './utils/provider';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App } from './App'
+import './App.scss'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Web3ReactProvider getLibrary={getProvider}>
-      <App />
-    </Web3ReactProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
